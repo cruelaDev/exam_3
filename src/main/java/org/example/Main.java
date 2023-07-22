86fb266479fbb88a92b3dd4fde5dac20d3dd924d
aziza <yuldasheva2210@gmail.com>
2023-07-22 16:26:37 +0500

    not connected

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static Scanner scannerStr = new Scanner(System.in);
 
     public static void main(String[] args) {
+//        Permission to cruelaDev/exam_3.git denied to developercruela.
         UI ui = new UI();
         ui.start();
     }
